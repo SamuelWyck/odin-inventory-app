1a4d299161d3c08709841d0a0475df639e3efe6d
samuel <samawyck@gmail.com>
2025-06-06 10:53:30 -0500

    Config server

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("node:path");
+const indexRoute = require("./routes/indexRoute.js");
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+const assetPath = path.join(__dirname, "public");
+
+app.use(express.static(assetPath));
+app.use(express.urlencoded({extended: true}));
+
+
+app.use("/", indexRoute);
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, function() {`Server listening on port ${PORT}!`});
