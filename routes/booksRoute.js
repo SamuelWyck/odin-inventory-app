023b7fddd48ee5418e18264d076f6bee2cf05e23
samuel <samawyck@gmail.com>
2025-06-10 13:26:03 -0500

    Hook up edit routes

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -7,6 +7,8 @@ const booksRoute = Router();
 booksRoute.get("/", booksController.booksGet);
 booksRoute.get("/books/new", booksController.newBookGet);
 booksRoute.post("/books/new", booksController.newBookPost);
+booksRoute.get("/books/edit/:bookId", booksController.editBookGet);
+booksRoute.post("/books/edit/", booksController.editBookPost)
 
 
 module.exports = booksRoute;
