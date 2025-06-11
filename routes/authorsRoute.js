d12d3306785b93d4b79150d1f36ee5384980198a
samuel <samawyck@gmail.com>
2025-06-11 12:20:29 -0500

    Hook up authors/new route

--- routes/authorsRoute.js
+++ routes/authorsRoute.js
@@ -6,6 +6,8 @@ const authorsController = require("../controllers/authorsController.js");
 const authorsRoute = Router();
 
 
+authorsRoute.get("/new", authorsController.newAuthorGet);
+authorsRoute.post("/new", authorsController.newAuthorPost);
 authorsRoute.get("/{:authorId}", authorsController.authorsGet);
 
 
