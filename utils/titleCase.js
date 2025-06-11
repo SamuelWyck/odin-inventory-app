75f12767e4a6d55a7cf68b9395c125523fff0385
samuel <samawyck@gmail.com>
2025-06-11 14:28:24 -0500

    Rework function to clean extra whitespace

--- utils/titleCase.js
+++ utils/titleCase.js
@@ -1,6 +1,7 @@
 function titleCase(string) {
     string = string.toLowerCase().trim();
     const words = string.split(" ");
+    const cleanedWords = [];
     const littleWords = new Set([
         "a", "an", "and", "as", "at", "but", "by",
         "for", "in", "nor", "of", "on", "or", "the",
@@ -8,17 +9,29 @@ function titleCase(string) {
     ]);
 
     for (let i = 0; i < words.length; i += 1) {
-        const word = words[i];
-        if (i === 0) {
-            words[i] = word[0].toUpperCase() + word.slice(1);
+        let word = words[i].trim();
+
+        if (word === "") {
             continue;
         }
-        if (!littleWords.has(word)) {
-            words[i] = word[0].toUpperCase() + word.slice(1);
+
+        if (i === 0) {
+            cleanedWords.push(
+                word.slice(0, 1).toUpperCase() + word.slice(1)
+            );
+            continue;
+        }
+
+        if (littleWords.has(word)) {
+            cleanedWords.push(word);
+        } else {
+            cleanedWords.push(
+                word.slice(0, 1).toUpperCase() + word.slice(1)
+            );
         }
     }
 
-    return words.join(" ");
+    return cleanedWords.join(" ");
 };
 
 
