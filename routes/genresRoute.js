7c0ed2f3199edf6fcbce060763f048ed5deb2398
samuel <samawyck@gmail.com>
2025-06-12 10:25:25 -0500

    Ad routes for editing genres

--- routes/genresRoute.js
+++ routes/genresRoute.js
@@ -8,6 +8,8 @@ const genresRoute = Router();
 
 genresRoute.get("/new", genresController.newGenreGet);
 genresRoute.post("/new", genresController.newGenrePost);
+genresRoute.get("/edit/:genreId", genresController.editGenreGet);
+genresRoute.post("/edit", genresController.editGenrePost);
 genresRoute.get("/{:genreId}", genresController.genresGet);
 
 
