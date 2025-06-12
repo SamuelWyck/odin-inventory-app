0b9a1f912d5b4faac12ed169e7bcfc5c47c61e4e
samuel <samawyck@gmail.com>
2025-06-12 11:17:31 -0500

    Add sslmode

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const {Pool} = require("pg");
 
 
 module.exports = new Pool({
-    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB}`
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB}?sslmode=require`
 });
